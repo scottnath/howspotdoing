f0bed3bbd0c23b036867d4c02426f4adc20ae690
Scott Nath <github@scottnath.com>
2026-01-29 23:43:01 -0500

    fix: site style cleanup

--- astro.config.ts
+++ astro.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'astro/config';
 import partytown from '@astrojs/partytown';
 
 export default defineConfig({
+  devToolbar: {
+    enabled: false,
+  },
   integrations: [partytown()],
   output: 'static',
 });
